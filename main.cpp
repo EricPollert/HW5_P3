e305a4472d254e0fbabff2a04e9499a723122b85
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-26 03:03:51 +0000

    Reorganize order of code

--- main.cpp
+++ main.cpp
@@ -18,16 +18,15 @@ int main() {
   K1 = 13.12;
   K2 = 11.37;
   K3 = 0.3965;
-
-  double WCI;
-  WCI = K1 + ((0.6125) * T) - (K2 * pow(W, 0.16)) + (K3 * T * (pow(W, 0.16)));
   
 
-  int i;
   // loops
-  for(int T = 2; T <= 10; T + 4){ 
-  for(int W = 5; W <= 11; W + 2){
+  for(int T = 2; T <= 10; T += 4){ 
+  for(int W = 5; W <= 11; W += 2){
+    double WCI;
+    WCI = K1 + ((0.6125) * T) - (K2 * pow(W, 0.16)) + (K3 * T * (pow(W, 0.16)));
     cout << WCI;
+    
   
     }
     }

681cf543e9411e81a473e3e1ce02b65a886d7941
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-26 01:34:02 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW5P3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  // initialize variables
+  
+  // conversion factors
+  double K1; 
+  double K2;
+  double K3;
+
+  double WCI;
+
+  K1 = 13.12;
+  K2 = 11.37;
+  K3 = 0.3965;
+
+  
+}

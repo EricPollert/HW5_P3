a90767ea976600ac25e50484ecb451ca4a40e95b
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-26 01:53:29 +0000

    Added equation to bottom and ints

--- .ccls-cache/@home@runner@HW5P3/main.cpp
+++ .ccls-cache/@home@runner@HW5P3/main.cpp
@@ -12,11 +12,19 @@ int main() {
   double K2;
   double K3;
 
+  int T;
+  int W;
+
   double WCI;
 
   K1 = 13.12;
   K2 = 11.37;
   K3 = 0.3965;
 
+  // loops
   
+
+
+
+  WCI = K1 + ((0.6125) * T) - (K2 * pow(W, 0.16)) + (K3 * T * (pow(W, 0.16)));
 }

0638a3ab6c91127380775f55e5c4395f034852f6
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-26 03:34:21 +0000

    Added formatting cout statement

--- main.cpp
+++ main.cpp
@@ -18,15 +18,16 @@ int main() {
   K1 = 13.12;
   K2 = 11.37;
   K3 = 0.3965;
-  
+
+  int counter;
 
   // loops
   for(int T = 2; T <= 10; T += 4){ 
   for(int W = 5; W <= 11; W += 2){
     double WCI;
     WCI = K1 + ((0.6125) * T) - (K2 * pow(W, 0.16)) + (K3 * T * (pow(W, 0.16)));
-    cout << WCI;
-    
+
+    cout << "| " << T << " | " << W << " | " << WCI << "\n";
   
     }
     }
